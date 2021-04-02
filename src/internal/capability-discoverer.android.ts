51e1de0fa95c89dee4e68394edb9bcec9820f332
Miguel <al341802@uji.es>
2021-04-02 12:17:30 +0200

    Add capability discoverer

---
+++ src/internal/capability-discoverer.android.ts
@@ -0,0 +1,30 @@
+import { android as androidApp } from "tns-core-modules/application/application";
+
+import OnCompleteListener = com.google.android.gms.tasks.OnCompleteListener;
+import { NodeSet, wearOS } from "./wear-os-types.android";
+
+export class CapabilityDiscoverer {
+
+    constructor(private capability: string) {
+    }
+
+    public async getAvailableNodes(): Promise<NodeSet> {
+        const capability = wearOS.Wearable.getCapabilityClient(androidApp.context)
+            .getCapability(this.capability, wearOS.CapabilityClient.FILTER_REACHABLE);
+
+        const capabilityPromise = new Promise<wearOS.CapabilityInfo>((resolve, reject) => {
+            capability.addOnCompleteListener(new OnCompleteListener({
+                onComplete: task => {
+                    if (task.isSuccessful()) {
+                        resolve(task.getResult());
+                    } else {
+                        reject(task.getException());
+                    }
+                }
+            }));
+        });
+
+        const capabilityResult = await capabilityPromise;
+        return capabilityResult.getNodes();
+    }
+}
