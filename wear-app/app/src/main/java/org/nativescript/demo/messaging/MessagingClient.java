f3065dc840aa83d425c406f735447d0ec4c94522
Miguel <al341802@uji.es>
2021-04-14 16:13:41 +0200

    wearapp: add messaging utilities

---
+++ wear-app/app/src/main/java/org/nativescript/demo/messaging/MessagingClient.java
@@ -0,0 +1,40 @@
+package org.nativescript.demo.messaging;
+
+import android.content.Context;
+
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.Wearable;
+
+public class MessagingClient {
+
+    private MessageClient messageClient;
+
+    public MessagingClient(Context context) {
+        this.messageClient = Wearable.getMessageClient(context);
+    }
+
+    public void sendSuccessfulResponse(String sourceNodeId, ResultMessagingProtocol protocol) {
+        messageClient.sendMessage(
+                sourceNodeId,
+                protocol.getMessagePath(),
+                protocol.getSuccessResponse().getBytes()
+        );
+    }
+
+    public void sendFailureResponse(String sourceNodeId, ResultMessagingProtocol protocol) {
+        messageClient.sendMessage(
+                sourceNodeId,
+                protocol.getMessagePath(),
+                protocol.getFailureResponse().getBytes()
+        );
+    }
+
+    public void sendFailureResponseWithReason(String sourceNodeId, ResultMessagingProtocol protocol, String failureReason) {
+        String message = protocol.getFailureResponse() + "#" + failureReason;
+        messageClient.sendMessage(
+                sourceNodeId,
+                protocol.getMessagePath(),
+                message.getBytes()
+        );
+    }
+}
