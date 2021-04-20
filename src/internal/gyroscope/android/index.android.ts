fad0536e1c724821ecb30892c1c8e4f4a9ea55e1
Miguel <al341802@uji.es>
2021-04-20 15:21:40 +0200

    Gyroscope support - protocol and accessors

---
+++ src/internal/gyroscope/android/index.android.ts
@@ -0,0 +1,26 @@
+import { CollectorManager } from "../../collector-manager";
+import { CollectorManagerImpl } from "../../collector-manager-impl.android";
+import { GyroscopeSensorRecord} from "../record";
+import { SensorCallbackManager } from "../../sensor-callback-manager";
+import { getGyroscopeRecordService} from "./record-messaging-service.android";
+import { buildGyroscopeMessagingProtocol } from "./protocol.android";
+import { MessagingClientImpl } from "../../messaging/android/messaging-client.android";
+import { getResultMessagingService } from "../../messaging/android/result-messaging-service.android";
+
+export function getAndroidGyroscopeCollector(): CollectorManager {
+    const protocol = buildGyroscopeMessagingProtocol();
+    const callback = new SensorCallbackManager<GyroscopeSensorRecord[]>("newGyroscopeRecord");
+    const messagingClient = new MessagingClientImpl(
+        protocol,
+        getResultMessagingService()
+    );
+    const recordService = getGyroscopeRecordService();
+    recordService.setProtocol(protocol);
+    recordService.setCallbackManager(callback);
+    return new CollectorManagerImpl<GyroscopeSensorRecord>(
+        protocol,
+        "gyroscope-sensor",
+        messagingClient,
+        callback,
+    );
+}
---
+++ src/internal/gyroscope/index.ts
@@ -0,0 +1,11 @@
+import { CollectorManager } from "../collector-manager";
+import { android as androidApp } from "tns-core-modules/application/application";
+import { getAndroidGyroscopeCollector } from "./android/index.android";
+
+export function getGyroscopeCollector(): CollectorManager {
+    if (androidApp) {
+        return getAndroidGyroscopeCollector();
+    } else {
+        return null;
+    }
+}
