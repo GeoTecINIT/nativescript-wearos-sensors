aa1f2bc8ff5e927097e855dfd97aac598f5141fe
Miguel <al341802@uji.es>
2021-04-20 15:23:39 +0200

    Update demo with gyroscope

--- demo/app/home/home-view-model.ts
+++ demo/app/home/home-view-model.ts
@@ -1,36 +1,70 @@
 import { Observable } from "tns-core-modules/data/observable";
 import { getAccelerometerCollector } from "../../../src/internal/accelerometer";
 import { CollectorManager } from "../../../src/internal/collector-manager";
+import { getGyroscopeCollector } from "../../../src/internal/gyroscope";
 
 export class HomeViewModel extends Observable {
 
     private accelerometerCollector: CollectorManager;
+    private gyroscopeCollector: CollectorManager;
 
     constructor() {
         super();
         this.accelerometerCollector = getAccelerometerCollector();
+        this.gyroscopeCollector = getGyroscopeCollector();
         this.accelerometerCollector.listenSensorUpdates((accelerometerRecords) => {
             console.log(JSON.stringify(accelerometerRecords));
         });
+        this.gyroscopeCollector.listenSensorUpdates((gyroscopeRecords) => {
+            console.log(JSON.stringify(gyroscopeRecords));
+        });
     }
 
-    onSendIsReadyTap() {
+    onAccIsReadyTap() {
         this.accelerometerCollector.isReady().then((ready) => {
-            console.log("isReadyResponse: " + ready);
+            console.log("acc isReadyResponse: " + ready);
         })
     }
 
-    onSendPrepareTap() {
+    onAccPrepareTap() {
         this.accelerometerCollector.prepare().then((prepareErrors) => {
-            console.log(`prepareResponse, errors: ${JSON.stringify(prepareErrors)}`);
+            console.log(`acc prepareResponse, errors: ${JSON.stringify(prepareErrors)}`);
         })
     }
 
-    onSendStartTap() {
+    onAccStartTap() {
         this.accelerometerCollector.startCollecting();
     }
 
-    onSendStopTap() {
+    onAccStopTap() {
         this.accelerometerCollector.stopCollecting();
+    }
+
+    onAccClearNodesTap() {
+        this.accelerometerCollector.clearNodes();
+    }
+
+    onGyroIsReadyTap() {
+        this.gyroscopeCollector.isReady().then((ready) => {
+            console.log("gyro isReadyResponse: " + ready);
+        })
+    }
+
+    onGyroPrepareTap() {
+        this.gyroscopeCollector.prepare().then((prepareErrors) => {
+            console.log(`gyro prepareResponse, errors: ${JSON.stringify(prepareErrors)}`);
+        })
+    }
+
+    onGyroStartTap() {
+        this.gyroscopeCollector.startCollecting();
+    }
+
+    onGyroStopTap() {
+        this.gyroscopeCollector.stopCollecting();
+    }
+
+    onGyroClearNodesTap() {
+        this.gyroscopeCollector.clearNodes();
     }
 }
