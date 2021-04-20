00cb6ff59bf7776c16a5d496a4b66d58bf7f3639
Miguel <al341802@uji.es>
2021-04-20 15:16:09 +0200

    wearapp: gyroscope support - fixes in collector and last changes

--- wear-app/app/src/main/java/org/nativescript/demo/records/accumulator/RecordAccumulatorProvider.java
+++ wear-app/app/src/main/java/org/nativescript/demo/records/accumulator/RecordAccumulatorProvider.java
@@ -1,6 +1,6 @@
 package org.nativescript.demo.records.accumulator;
 
-import org.nativescript.demo.records.AccelerometerRecord;
+import org.nativescript.demo.records.TriAxialRecord;
 import org.nativescript.demo.records.callbacks.AbstractRecordCallback;
 import org.nativescript.demo.sensoring.WearSensor;
 
@@ -10,7 +10,8 @@ public class RecordAccumulatorProvider {
     public static RecordAccumulator getRecordAccumulatorProviderFor(WearSensor wearSensor, AbstractRecordCallback callback, int limit) {
         switch (wearSensor) {
             case ACCELEROMETER:
-                return new RecordAccumulator<AccelerometerRecord>(callback, limit);
+            case GYROSCOPE:
+                return new RecordAccumulator<TriAxialRecord>(callback, limit);
             default:
                 return null;
         }
--- wear-app/app/src/main/java/org/nativescript/demo/sensoring/CollectorManager.java
+++ wear-app/app/src/main/java/org/nativescript/demo/sensoring/CollectorManager.java
@@ -3,28 +3,28 @@ package org.nativescript.demo.sensoring;
 import android.content.Context;
 import android.hardware.SensorEventListener;
 
-import org.nativescript.demo.listeners.WearSensorListener;
 import org.nativescript.demo.listeners.SensorListenerProvider;
 import org.nativescript.demo.records.accumulator.RecordAccumulator;
 import org.nativescript.demo.records.accumulator.RecordAccumulatorProvider;
 import org.nativescript.demo.records.callbacks.AbstractRecordCallback;
 import org.nativescript.demo.records.callbacks.RecordCallbackProvider;
 
+import java.util.HashMap;
+
 public class CollectorManager {
 
     private Context context;
     private WearSensorManager wearSensorManager;
 
+    private HashMap<WearSensor, SensorEventListener> listeners;
+
     public CollectorManager(Context context) {
         this.context = context;
         this.wearSensorManager = new WearSensorManager(context);
+        this.listeners = new HashMap<>();
     }
 
     public boolean startCollectingFrom(WearSensor wearSensor, String requesterId, String sendingPath) {
-        WearSensorListener listener = SensorListenerProvider.getListenerAndNotifierFor(wearSensor);
-        if (listener == null)
-            return false;
-
         AbstractRecordCallback callback = RecordCallbackProvider.getRecordCallbackFor(
                 wearSensor,
                 context,
@@ -37,13 +37,21 @@ public class CollectorManager {
                 callback,
                 50);
 
-        listener.setRecordAccumulator(accumulator);
+        SensorEventListener listener = SensorListenerProvider.getListenerAndNotifierFor(wearSensor, accumulator);
+        if (listener == null)
+            return false;
+
+        listeners.put(wearSensor, listener);
 
         return wearSensorManager.startCollectingFrom(wearSensor, listener);
     }
 
     public void stopCollectingFrom(WearSensor wearSensor) {
-        SensorEventListener listener = SensorListenerProvider.getListenerAndNotifierFor(wearSensor);
+        SensorEventListener listener = listeners.get(wearSensor);
+        if (listener == null)
+            return;
+
+        listeners.remove(wearSensor);
         wearSensorManager.stopCollectingFrom(wearSensor, listener);
     }
 }
