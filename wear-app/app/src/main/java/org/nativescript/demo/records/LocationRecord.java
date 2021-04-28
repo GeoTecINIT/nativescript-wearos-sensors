a7e45a2d68cc3202137c7bf80dc798d23efe25d2
Miguel <al341802@uji.es>
2021-04-28 11:58:16 +0200

    wearapp: Add location record and callback

---
+++ wear-app/app/src/main/java/org/nativescript/demo/records/LocationRecord.java
@@ -0,0 +1,27 @@
+package org.nativescript.demo.records;
+
+public class LocationRecord extends Record {
+
+    private double latitude;
+    private double longitude;
+    private double altitude;
+
+    public LocationRecord(long timestamp, double latitude, double longitude, double altitude) {
+        super(timestamp);
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+}
