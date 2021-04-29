7c9db7e6c02328293689f68297dae0821101b677
Miguel <al341802@uji.es>
2021-04-29 12:43:00 +0200

    Add location protocol and collector providers

--- src/internal/sensors/index.ts
+++ src/internal/sensors/index.ts
@@ -3,6 +3,7 @@ import { CollectorManager } from "../collector-manager";
 import { getAccelerometerCollector } from "./triaxial/accelerometer";
 import { getGyroscopeCollector } from "./triaxial/gyroscope";
 import { getMagnetometerCollector } from "./triaxial/magnetometer";
+import { getLocationCollector } from "./location";
 
 export function getSensorCollector(wearSensor: SensorType): CollectorManager {
     switch (wearSensor) {
@@ -12,6 +13,8 @@ export function getSensorCollector(wearSensor: SensorType): CollectorManager {
             return getGyroscopeCollector();
         case SensorType.MAGNETOMETER:
             return getMagnetometerCollector();
+        case SensorType.LOCATION:
+            return getLocationCollector();
         default:
             return null;
     }
