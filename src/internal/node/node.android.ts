5d96f894b9fc434939912c3293ea5d54cbb0cda8
Miguel <al341802@uji.es>
2021-10-18 10:40:54 +0200

    [mobile]: Replace node manager with node entity

---
+++ src/internal/node/node.android.ts
@@ -0,0 +1,25 @@
+import { SensorType } from "../sensors/sensor-type";
+
+export class Node {
+
+    constructor(
+        private _id: string,
+        private _name: string,
+        private _capabilities: SensorType[],
+    ) {
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get capabilities(): SensorType[] {
+        return this._capabilities;
+    }
+}
+
+
