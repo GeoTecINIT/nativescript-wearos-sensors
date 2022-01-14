3a07956e098dad7437b27c9d892008b2853503f3
Miguel <al341802@uji.es>
2022-01-14 12:06:22 +0100

    [mobile]: comments to task graph

--- demo/app/home/tasks/graph.ts
+++ demo/app/home/tasks/graph.ts
@@ -5,6 +5,7 @@ class DemoTaskGraph implements TaskGraph {
         on: EventListenerGenerator,
         run: RunnableTaskDescriptor
     ): Promise<void> {
+        // Commands logs
         on("startAllCommand", run("commandLoggerTask"));
         on("stopAllCommand", run("commandLoggerTask"));
         on("startAccelerometerCommand", run("commandLoggerTask"));
@@ -18,31 +19,37 @@ class DemoTaskGraph implements TaskGraph {
         on("startLocationCommand", run("commandLoggerTask"));
         on("stopLocationCommand", run("commandLoggerTask"));
 
+        // Accelerometer
         on("startAllCommand", run("accelerometerStartSensorTask"));
         on("startAccelerometerCommand", run("accelerometerStartSensorTask"));
         on("stopAccelerometerCommand", run("accelerometerStopSensorTask"));
         on("stopAllCommand", run("accelerometerStopSensorTask"));
 
+        // Gyroscope
         on("startAllCommand", run("gyroscopeStartSensorTask"));
         on("startGyroscopeCommand", run("gyroscopeStartSensorTask"));
         on("stopGyroscopeCommand", run("gyroscopeStopSensorTask"));
         on("stopAllCommand", run("gyroscopeStopSensorTask"));
 
+        // Magnetometer
         on("startAllCommand", run("magnetometerStartSensorTask"));
         on("startMagnetometerCommand", run("magnetometerStartSensorTask"));
         on("stopMagnetometerCommand", run("magnetometerStopSensorTask"));
         on("stopAllCommand", run("magnetometerStopSensorTask"));
 
+        // Heart Rate
         on("startAllCommand", run("heartRateStartSensorTask"));
         on("startHeartRateCommand", run("heartRateStartSensorTask"));
         on("stopHeartRateCommand", run("heartRateStopSensorTask"));
         on("stopAllCommand", run("heartRateStopSensorTask"));
 
+        // Location
         on("startAllCommand", run("locationStartSensorTask"));
         on("startLocationCommand", run("locationStartSensorTask"));
         on("stopLocationCommand", run("locationStopSensorTask"));
         on("stopAllCommand", run("locationStopSensorTask"));
 
+        // Records logs
         on("accelerometerRecordsAcquired", run("recordLoggerTask"));
         on("gyroscopeRecordsAcquired", run("recordLoggerTask"));
         on("magnetometerRecordsAcquired", run("recordLoggerTask"));
