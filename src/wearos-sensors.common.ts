c36f673633c96a7949c545c35a0a5fa4b7c8dff2
Miguel Matey Sanz <al341802@uji.es>
2022-04-19 10:59:05 +0200

    [mobile]: expose isReady and prepare methods

--- src/wearos-sensors.common.ts
+++ src/wearos-sensors.common.ts
@@ -17,4 +17,12 @@ export class Common extends Observable {
   public emitEvent(eventName: string, eventData?: EventData) {
       taskDispatcher.emitEvent(eventName, eventData);
   }
+
+  public isReady(): Promise<boolean> {
+      return taskDispatcher.isReady();
+  }
+
+  public prepare(): Promise<void> {
+      return taskDispatcher.prepare();
+  }
 }
