fd730759ea2503cd2a8ed6f72be9caed9e244bb7
Miguel Matey Sanz <al341802@uji.es>
2022-06-27 15:46:35 +0200

    chore(plugin): remove NTD unused dependency

--- src/wearos-sensors.common.ts
+++ src/wearos-sensors.common.ts
@@ -1,21 +1,7 @@
 import { Observable } from "@nativescript/core";
-import { taskDispatcher } from "nativescript-task-dispatcher";
-import { EventData } from "nativescript-task-dispatcher/events";
 
 
 export class Common extends Observable {
   init(): void {
-  }
-
-  public emitEvent(eventName: string, eventData?: EventData) {
-      taskDispatcher.emitEvent(eventName, eventData);
-  }
-
-  public isReady(): Promise<boolean> {
-      return taskDispatcher.isReady();
-  }
-
-  public prepare(): Promise<void> {
-      return taskDispatcher.prepare();
   }
 }
