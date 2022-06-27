12ad93836cb8f1a47b2b57bcd5720e2c077a72ca
Miguel Matey Sanz <al341802@uji.es>
2022-06-27 15:42:19 +0200

    refactor(plugin): remove remaining send message task

--- src/internal/tasks/index.ts
+++
@@ -1,6 +0,0 @@
-import { Task } from "nativescript-task-dispatcher/tasks";
-import { SendSingleMessageTask } from "./send-single-message-task";
-
-export const internalTasks: Array<Task> = [
-    new SendSingleMessageTask(),
-];
